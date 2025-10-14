0df87a947cee3eb3e08ed936da982a4f9343062f
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-10-14 23:59:53 +0200

    añadir paginas home e instalaciones

---
+++ hotel-six-senses-ts/src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import { HomeNav } from '@/components/HomeNav';
+
+
+const Home = () => {
+    /* para el hover podria usar onMouseEnter y onMouseLeave */
+
+    return (<>
+
+        <div className="Home-img">
+            <div className='Home-title'>
+                <h1 className="Home-h1">hotel Six Senses</h1>
+                <h2 className="Home-h2"><i>Ibiza</i> </h2>
+            </div>
+            <HomeNav />
+        </div >
+
+    </>);
+}
+
+export default Home;
---
+++ hotel-six-senses-ts/src/pages/Instalaciones.tsx
@@ -0,0 +1,35 @@
+import { instalaciones } from "@/data/instalaciones";
+import { useScrollToTop } from "@/hooks/useScrollToTop";
+
+
+const Instalaciones = () => {
+
+    /* reset de scroll para cuando entre en una hab individual no este el scroll bajo */
+    useScrollToTop();
+
+    return (
+
+        <>
+            <div className="GalleryInstalaciones">
+                {instalaciones.map((inst, i) => (
+                    <div key={inst.id} className={`GalleryInstalaciones-item ${inst.size} ${i % 2 === 0 ? 'left' : 'right'}`}>
+                        <div className='ImageInstalaciones-container'>
+                            <img className="ImageInstalaciones-default" src={inst.default} alt="img instalacion 1" />
+                            <img className="ImageInstalaciones-hover" src={inst.hover} alt="img instalacion 2" />
+                        </div>
+
+                        <div className="Image-legend">
+                            <span className="Legend-title">{inst.name} </span>
+                            <p className="Legend-description">{inst.description} </p>
+
+                        </div>
+                    </div>
+
+                ))}
+
+            </div>
+        </>
+    );
+}
+
+export default Instalaciones;
