088554f2e7b3d76c731e06b409b3587c1aca233a
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-10-20 14:26:58 +0200

    esclarecer uso de datos externos en el Readme

--- hotel-six-senses-ts/src/pages/Reservar.tsx
+++ hotel-six-senses-ts/src/pages/Reservar.tsx
@@ -88,10 +88,10 @@ const Reservar = () => {
                 </form>
                 {showModal && (
                     <div className="Booking-modal">
-                        <h2>¡Enhorabuena {nameGuest}, has completado tu reserva en Six Senses!</h2>
-                        <p>Fechas de la reserva: <span>{checkIn?.toLocaleDateString()} {checkOut?.toLocaleDateString()}</span></p>
-                        <p>Te hemos enviado los datos de confirmación a {emailGuest}</p>
-                        <button onClick={handleCloseModal}>Cerrar</button>
+                        <h2 className="Booking-titleModal">¡Enhorabuena {nameGuest}, has completado tu reserva en Six Senses!</h2>
+                        <p className="Booking-textModal">Fechas de la reserva del <span className="Booking-dateModal">{checkIn?.toLocaleDateString()}</span> al <span className="Booking-dateModal">{checkOut?.toLocaleDateString()}</span></p>
+                        <p className="Booking-textModal">Te hemos enviado los datos de confirmación a {emailGuest}</p>
+                        <button className="Booking-btn" onClick={handleCloseModal}>Cerrar</button>
 
 
                     </div>
