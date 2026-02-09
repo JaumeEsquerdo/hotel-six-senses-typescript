9df8207ce25d284a06d4fe3f90dc3fd77d2e6974
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-02-09 15:54:27 +0100

    añadir onboarding con transicion de entrada tipada con framer motion

---
+++ hotel-six-senses-ts/src/components/Onboarding.tsx
@@ -0,0 +1,66 @@
+import "@/css/onboarding.css";
+import { motion, type Variants, type Transition } from "framer-motion";
+
+const variantsH1: Variants = {
+    hidden: { opacity: 0 },
+    visible: {
+        opacity: 1,
+        transition: { delayChildren: 0.4, staggerChildren: 0.6 },
+    },
+};
+
+const spansVariants: Variants = {
+    hidden: { opacity: 0, y: 200 },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: { duration: 0.6, ease: "easeOut" },
+    },
+};
+
+const layoutTransition: Transition = {
+    type: "tween",
+    ease: "easeInOut",
+    duration: 2.5, // Esto controla la lentitud del VIAJE al Home
+};
+
+export const Onboarding = () => {
+    return (
+        <div className="Onboarding">
+            <div className="Onboarding-title">
+                <motion.h1
+                    layoutId="main-title"
+                    variants={variantsH1}
+                    initial="hidden"
+                    animate="visible"
+                    transition={layoutTransition}
+                    className="Onboarding-h1"
+                >
+                    <motion.span className="Onboarding-span" variants={spansVariants}>
+                        hotel
+                    </motion.span>{" "}
+                    <motion.span className="Onboarding-span" variants={spansVariants}>
+                        Six
+                    </motion.span>{" "}
+                    <motion.span className="Onboarding-span" variants={spansVariants}>
+                        Senses
+                    </motion.span>
+                </motion.h1>
+                <motion.h2
+                    layoutId="sub-title"
+                    className="Onboarding-h2"
+                    initial={{ opacity: 0, y: 200 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                        delay: 2.2,
+                        duration: 0.6,
+                        ease: "easeOut",
+                        layout: layoutTransition,
+                    }}
+                >
+                    <i>Ibiza</i>{" "}
+                </motion.h2>
+            </div>
+        </div>
+    );
+};
--- hotel-six-senses-ts/src/pages/Home.tsx
+++ hotel-six-senses-ts/src/pages/Home.tsx
@@ -1,20 +1,53 @@
-import { HomeNav } from '@/components/HomeNav';
-
+import { HomeNav } from "@/components/HomeNav";
+import { Onboarding } from "@/components/Onboarding.tsx";
+import { useEffect, useState } from "react";
+import { motion, type Transition } from "framer-motion";
 
 const Home = () => {
-    /* para el hover podria usar onMouseEnter y onMouseLeave */
+    const [showHome, setShowHome] = useState(false);
 
-    return (<>
+    const layoutTransition: Transition = {
+        type: "tween",
+        ease: "easeInOut",
+        duration: 0.7, // Esto controla la lentitud del VIAJE al Home
+    };
 
-        <div className="Home-img">
-            <div className='Home-title'>
-                <h1 className="Home-h1">hotel Six Senses</h1>
-                <h2 className="Home-h2"><i>Ibiza</i> </h2>
-            </div>
-            <HomeNav />
-        </div >
+    //efecto delay para cambiar de onboarding inicial al home
+    useEffect(() => {
+        const onboardingDelay = setTimeout(() => {
+            setShowHome(true);
+        }, 4000);
 
-    </>);
-}
+        return () => clearTimeout(onboardingDelay);
+    }, []);
+
+    return (
+        <>
+            {!showHome ? (
+                <Onboarding />
+            ) : (
+                <div className="Home-img">
+                    <div className="Home-title">
+                        <motion.h1
+                            layoutId="main-title"
+                            transition={layoutTransition}
+                            className="Home-h1"
+                        >
+                            hotel Six Senses
+                        </motion.h1>
+                        <motion.h2
+                            layoutId="sub-title"
+                            transition={layoutTransition}
+                            className="Home-h2"
+                        >
+                            <i>Ibiza</i>{" "}
+                        </motion.h2>
+                    </div>
+                    <HomeNav />
+                </div>
+            )}
+        </>
+    );
+};
 
 export default Home;
